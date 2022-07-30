9d8e7d189da81630d25716dacd393dc57481c532
guzyt <guslink02@gmail.com>
2022-07-30 16:48:56 -0300

    Add files via upload

---
+++ stats-Nades&HS-counter.py
@@ -0,0 +1,9 @@
+for  rounds in json_dict['stats']['TsRounds']:
+	hs=0
+	nades=0
+	for hits in rounds['HitLog']:
+		if hits['Damage'] == 51:
+			hs+=1
+	for nds in rounds['KillLog']:
+		if nds['Weapon'] == 'HE':
+			nades+=1
