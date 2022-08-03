0fc5568dd908f3b03c43f466819533a948439ec9
guzyt <guslink02@gmail.com>
2022-08-02 22:25:20 -0300

    Update stats-Streaks.py
    seems more right

--- stats-Streaks.py
+++ stats-Streaks.py
@@ -1,29 +1,26 @@
+		
 streak = 0
-antistreak =0			
+antistreak = 0           
 continuation = 0
 absence = 0
-i=0
+for i in range(0, len(player['TS']['Kills'])-2):
+    if player['TS']['Kills'][i] and player['TS']['Kills'][i+1]: #if kills in consecutive rounds exist:
+	continuation += 1
 
-while i < (len(player['TS']['Kills'])-1):
-	if player['TS']['Kills'][i] and player['TS']['Kills'][i+1]: #if kills in consecutive rounds exist:
-		continuation+=1
-		
-		if continuation > streak: 	# growing
-			streak=continuation		
-		
-	elif player['TS']['Kills'][i] == 0 and player['TS']['Kills'][i+1] == 0:				 
-		absence+=1
+	if continuation > streak:   # growing
+	    streak=continuation     
 
-		if absence > antistreak:
-			antistreak=absence
+    elif player['TS']['Kills'][i] == 0 and player['TS']['Kills'][i+1] == 0:              
+	absence += 1
 
-	else:
-		continuation = 0
-		absence = 0 
-		
-	i+=1
+	if absence > antistreak:
+	    antistreak=absence
 
-streak+=1
-antistreak+=1
+    else:
+	continuation = 0
+	absence = 0 
+
+streak += 1
+antistreak += 1
 
 #antistreak and streak must be increased by one at the end
